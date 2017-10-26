959d32cbe4d03d67f8e9d7f78c8e6bc9724b1cd6
ihsansatriawan <ihsan.satriawan.20@gmail.com>
2017-10-26 21:07:25 +0700

    Implement apollo engine

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import config from 'config';
 import cors from 'cors';
 import { graphqlExpress, graphiqlExpress } from 'graphql-server-express';
+import { Engine } from 'apollo-engine';
 import bodyParser from 'body-parser';
 import schema from './schema';
 import logger from './lib/logger';
@@ -14,7 +15,26 @@ app.use('*', cors());
 
 app.use('/graphql', bodyParser.json(), graphqlExpress({
   schema,
+  tracing: true,
+  cacheControl: true,
 }));
+
+if (process.env.NODE_ENV === 'production') {
+  const engine = new Engine({
+    engineConfig: {
+      apiKey: config.GRAPHQL_ENGINE.API_KEY,
+      logging: {
+        level: 'DEBUG' 
+      }
+    },
+    graphqlPort: process.env.PORT || config.get('port'),  
+    endpoint: '/graphql',
+    dumpTraffic: true     
+  });
+  
+  engine.start();
+  app.use(engine.expressMiddleware());
+}
 
 if (process.env.NODE_ENV === 'dev') {
   app.use('/graphiql', graphiqlExpress({
